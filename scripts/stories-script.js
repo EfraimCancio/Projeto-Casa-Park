ef6f7882f5548e343025fff4e50a867e24289c0c
Efraim Cancio <efraimfonseca@gmail.com>
2021-09-16 12:49:19 -0300

    Ajustes solicitados pela desaigner

--- scripts/stories-script.js
+++ scripts/stories-script.js
@@ -1,10 +1,20 @@
-const open = document.getElementById('open-modal'); 
-const modal = document.getElementById('stories-modal'); 
-const close = document.getElementById('close-modal');
-
-open.addEventListener('click', () => {
-    modal.classList.add('show');
+const modal01 = document.getElementById('stories-modal-left01'); 
+const open01 = document.getElementById('open-modal01');
+const close01 = document.getElementById('close-modal01');
+open01.addEventListener('click', () => {
+    modal01.classList.add('show');
 })
-close.addEventListener('click', () => {
-    modal.classList.remove('show');
+close01.addEventListener('click', () => {
+    modal01.classList.remove('show');
+})
+
+const modal02 = document.getElementById('stories-modal-right01');
+const open02 = document.getElementById('open-modal02');
+const close02 = document.getElementById('close-modal02');
+
+open02.addEventListener('click', () => {
+    modal02.classList.add('show');
+})
+close02.addEventListener('click', () => {
+    modal02.classList.remove('show');
 })
