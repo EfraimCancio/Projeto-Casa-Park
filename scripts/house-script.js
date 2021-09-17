4bbf90dd8d3cd28481ab41fdcbd1adc0d4912832
Romulo Gomes <romulo.redg@gmail.com>
2021-09-16 21:42:26 -0300

    add modal a página house

--- scripts/house-script.js
+++ scripts/house-script.js
@@ -1,10 +1,14 @@
-const open = document.getElementById('open-modal'); 
-const modal = document.getElementById('house-modal'); 
-const close = document.getElementById('close-modal');
+const open1 = document.getElementById('open-modal1');
+const modal1 = document.getElementById('house-modal1');
+const close1 = document.getElementById('close-modal1');
 
-open.addEventListener('click', () => {
-    modal.classList.add('show');
+open1.addEventListener('click', () => {
+    modal1.classList.add('show');
 })
-close.addEventListener('click', () => {
-    modal.classList.remove('show');
+close1.addEventListener('click', () => {
+    modal1.classList.add('hide');
+    setTimeout(function () {
+        modal1.classList.remove('show');
+        modal1.classList.remove('hide');
+    }, 1000);
 })
