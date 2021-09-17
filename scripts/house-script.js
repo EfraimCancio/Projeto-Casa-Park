dfb60d0525c5e321058fc13d6a524b8fa9643a43
Efraim Cancio <efraimfonseca@gmail.com>
2021-09-16 22:06:09 -0300

    configuração das house-modals

--- scripts/house-script.js
+++ scripts/house-script.js
@@ -1,7 +1,6 @@
 const open1 = document.getElementById('open-modal1');
 const modal1 = document.getElementById('house-modal1');
 const close1 = document.getElementById('close-modal1');
-
 open1.addEventListener('click', () => {
     modal1.classList.add('show');
 })
@@ -10,5 +9,33 @@ close1.addEventListener('click', () => {
     setTimeout(function () {
         modal1.classList.remove('show');
         modal1.classList.remove('hide');
+    }, 1000);
+})
+
+const open2 = document.getElementById('open-modal2');
+const modal2 = document.getElementById('house-modal2');
+const close2 = document.getElementById('close-modal2');
+open2.addEventListener('click', () => {
+    modal2.classList.add('show');
+})
+close2.addEventListener('click', () => {
+    modal2.classList.add('hide');
+    setTimeout(function () {
+        modal2.classList.remove('show');
+        modal2.classList.remove('hide');
+    }, 1000);
+})
+
+const open3 = document.getElementById('open-modal3');
+const modal3 = document.getElementById('house-modal3');
+const close3 = document.getElementById('close-modal3');
+open3.addEventListener('click', () => {
+    modal3.classList.add('show');
+})
+close3.addEventListener('click', () => {
+    modal3.classList.add('hide');
+    setTimeout(function () {
+        modal3.classList.remove('show');
+        modal3.classList.remove('hide');
     }, 1000);
 })
